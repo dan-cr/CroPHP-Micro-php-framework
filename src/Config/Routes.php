1f05477506e36e3564d8f63c457039fdaef411dd
daniel <daniel.croucher@outlook.com>
2020-07-20 00:27:51 +0000

    Remove line breaks in routes

--- src/Config/Routes.php
+++ src/Config/Routes.php
@@ -1,21 +1,10 @@
 <?php
 
+// Disable type coercion and enforces scalar type hints
+declare(strict_types=1);
+
 use \Crodev\Core\Router;
 
-// Home Page
-Router::addRoute('/', 'GET', [
-    'handler' => 'test',
-    'action' => 'index'
-]);
-
-// Test Page
-Router::addRoute('/test/',  'GET', [
-    'handler' => 'home',
-    'action' => 'index'
-]);
-
-// 404 Page
-Router::addRoute('/404/',  'GET', [
-    'handler' => 'error',
-    'action' => 'notFound'
-]);
+Router::addRoute('/', 'GET', ['handler' => 'test', 'action' => 'index']);               // Home
+Router::addRoute('/test/test/',  'GET', ['handler' => 'home', 'action' => 'index']);    // Test
+Router::addRoute('/404/',  'GET', ['handler' => 'error', 'action' => 'notFound']);      // Error
